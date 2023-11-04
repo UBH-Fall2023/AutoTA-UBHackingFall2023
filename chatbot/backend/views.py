c338d336f1cba3b7e5e77a69776ed16d20c7171d
sumanyuj <sumanyuj@buffalo.edu>
2023-11-04 17:44:52 -0400

    Within backend directory, created sendMessage() and displayMessage() to request data from backend (which in this case is hard-coded, since the API to CHATGPT is not yet implmeneted. When running test server, you will see a text box and when any user input is receieved, will output a standard hardcoded string that was fetched from the view.py file in backend

--- chatbot/backend/views.py
+++ chatbot/backend/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def chatbot(request):
+
+    if(request.method == "POST"):
+        data = json.loads(request.body.decode('utf-8'))
+        message = data.get('message')
+        response = "This is a test reponse. It should actually be from OPEN AI API"
+        return JsonResponse({'message': message, 'response': response})
 
     return render(request,"chatbox.html")
 
