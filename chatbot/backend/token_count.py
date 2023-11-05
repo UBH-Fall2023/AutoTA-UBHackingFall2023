455e78244f6cdf493f7d5b495509092bd9d84d63
ethanc9 <cethan988@gmail.com>
2023-11-04 21:05:04 -0400

    Added dark mode and light mode options

--- chatbot/backend/token_count.py
+++ chatbot/backend/token_count.py
@@ -1,8 +1,10 @@
 import tiktoken
 
+global max_tokens
+max_tokens = 500
+
 def count_tokens(text: str) -> int:
    """Counts the number of tokens in a text string."""
    encoding = tiktoken.encoding_for_model("gpt-4")
    tokens = encoding.encode(text)
    return len(tokens)
-
