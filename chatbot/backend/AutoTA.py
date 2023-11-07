a1b8cd04655f6d24f6b688ae2e83e4feb4466a77
puru-soni-04 <purusoni@buffalo.edu>
2023-11-07 18:02:56 -0500

    Remove unnecessary files and dependencies, and refactor AutoTA.py

--- chatbot/backend/AutoTA.py
+++ chatbot/backend/AutoTA.py
@@ -3,13 +3,11 @@ import os
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
-import markdown
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import openai
 import json
 import numpy as np
-from IPython.display import display, HTML
 
 
 class TextPreprocessor():
